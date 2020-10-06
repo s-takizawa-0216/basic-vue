10636088e9028cdb3aab379234570484ff1ab394
s-takizawa <s.takizawa.0216@gmail.com>
2020-10-06 23:46:15 +0900

    add:props downの実装

---
+++ src/components/Section3/Fruits.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <FruitsList 
+      :fruitsItems=fruitsItems>
+    </FruitsList>
+  </div>
+</template>
+
+<script>
+import FruitsList from './FruitsList'
+
+export default { 
+  name: 'Fruits',
+  data: function() {
+    return {
+      fruitsItems: [
+        {name: 'apple'},
+        {name: 'orange'}
+      ]
+    }
+  },
+  components:{
+    FruitsList,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Section3/FruitsList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="fruit in fruitsItems" :key="fruit.name">
+        {{ fruit.name }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default { 
+  name: 'FruitsList',
+  props:{
+    fruitsItems: {
+      type: Array,
+      required: true,
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
