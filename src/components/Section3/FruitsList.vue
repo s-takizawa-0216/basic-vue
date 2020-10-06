be9cc024243a118587f0a7d3e6b2b866a0fc8913
s-takizawa <s.takizawa.0216@gmail.com>
2020-10-07 00:09:28 +0900

    add:event up の実装

--- src/components/Section3/FruitsList.vue
+++ src/components/Section3/FruitsList.vue
@@ -4,19 +4,36 @@
       <li v-for="fruit in fruitsItems" :key="fruit.name">
         {{ fruit.name }}
       </li>
+      合計: {{ total }} 個
     </ol>
+    <CounterButton @increment="incrementCartStatus()" />
   </div>
 </template>
 
 <script>
+import CounterButton from './CounterButton'
+
 export default { 
   name: 'FruitsList',
+  data: function() {
+    return {
+      total: 0,
+    }
+  },
+  methods: {
+    incrementCartStatus: function() {
+      this.total += 1;
+    }
+  },
   props:{
     fruitsItems: {
       type: Array,
       required: true,
     }
   },
+  components:{
+    CounterButton,
+  }
 }
 </script>
 
