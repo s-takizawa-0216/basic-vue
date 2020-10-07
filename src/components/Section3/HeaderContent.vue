95e2caad57abe3b9a13bf89307da5251cb1ac045
s-takizawa <s.takizawa.0216@gmail.com>
2020-10-07 23:04:33 +0900

    add:slotの基本形・使い方

---
+++ src/components/Section3/HeaderContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style='color: blue;'>
+    <slot name='content'>※親から何も来ない場合に表示</slot>
+  </div>
+</template>
+
+<script>
+
+
+export default { 
+  name: 'HeaderContent',
+  data: function() {
+    return {
+    
+    }
+  },
+  components:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Section3/HeaderItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style='color: gray;'>
+    <slot name='header'>※親から何も来ない場合に表示</slot>
+  </div>
+</template>
+
+<script>
+
+
+export default { 
+  name: 'Fruits',
+  data: function() {
+    return {
+    
+    }
+  },
+  components:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
