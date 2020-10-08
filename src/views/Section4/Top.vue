fe118a7b5d24a7f88df821a0dd7e2bf11c0cab4d
s-takizawa <s.takizawa.0216@gmail.com>
2020-10-08 23:08:46 +0900

    add:トップページとuer詳細ページの追加

---
+++ src/views/Section4/Top.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div class='link'>
+      <router-link to='/section4/top'>セクション4のTopへ</router-link>
+    </div>
+    <div class='link'>
+      <router-link to='/section4/users'>ユーザー一覧</router-link>
+    </div>
+    <router-view />
+  </nav>
+</template>
+
+<script>
+
+export default { 
+  name: 'Top',
+  data: function() {
+    return {
+    }
+  },
+  components:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  margin: 10px;
+}
+
+</style>
