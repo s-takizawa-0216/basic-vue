35163d34016a19b42757667b7fe96551d6329fd7
s-takizawa <s.takizawa.0216@gmail.com>
2020-10-10 22:00:08 +0900

    add:ユーザー一覧の追加

--- src/components/Section4/UserList.vue
+++ src/components/Section4/UserList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    ユーザー
+    <p>ユーザー一覧ページです</p>
+    <div class='loading' v-if='loading'>読み込み中</div>
+    <div class='error' v-if='error'>{{ error }}</div>
+    <div v-for='user in users' :key='user.id'>
+      {{ user.name }}
+    </div>
     <div>
       <router-link to='/section4/top'>セクション4のトップへ</router-link>
     </div>
@@ -8,14 +13,45 @@
 </template>
 
 <script>
+var getUsers = function(callback) {
+  setTimeout(function(){
+    callback(null, [
+      {
+        id: 1,
+        name: 'taro'
+      },
+      {
+        id: 2,
+        name: 'jiro'
+      }
+    ], 1000)
+  })
+}
 
 export default { 
   name: 'UserList',
   data: function() {
     return {
+      loading: false,
+      users: Array,
+      error: null,
     }
   },
-  components:{
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: function() {
+      this.loading = true;
+      getUsers(function(error, users) {
+        this.loading = false;
+        if(error) {
+          this.error = error;
+        } else{
+          this.users = users
+        }
+      }.bind(this))
+    }
   }
 }
 </script>
